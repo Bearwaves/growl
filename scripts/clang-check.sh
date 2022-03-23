d792202f5e024128dc21b7f7b48ca0f93646be92
Joel Auterson <joel.auterson@gmail.com>
2022-03-23 22:30:38 +0000

    Add clang-check.sh script

---
+++ scripts/clang-check.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
+git ls-files "${script_dir}/.." | grep -v 'contrib/' | grep -e '\.h$' -e '\.cpp$' -e '\.mm$' | xargs clang-check
