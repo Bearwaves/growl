070e7c977a5abdf1d75b677d11c1370715d73bc5
Joel Auterson <joel.auterson@gmail.com>
2022-10-15 13:46:19 +0100

    Add Android platform

---
+++ src/platforms/android/src/main.cpp
@@ -0,0 +1,143 @@
+#include "growl/core/api/api.h"
+#include "growl/core/api/api_internal.h"
+#include "growl/core/game/game.h"
+#include "growl/core/graphics/window.h"
+#include "growl/core/log.h"
+#include <android/asset_manager.h>
+#include <android/log.h>
+#include <android/native_window.h>
+#include <android_native_app_glue.h>
+#include <memory>
+
+using Growl::API;
+using Growl::AudioAPIInternal;
+using Growl::GraphicsAPIInternal;
+using Growl::LogLevel;
+using Growl::SystemAPIInternal;
+using Growl::WindowConfig;
+
+void initAndroidPlugin(API& api, android_app* state);
+void initSoLoudPlugin(API& api);
+void initOpenGLPlugin(API& api);
+std::unique_ptr<Growl::Game> createGame();
+
+void enableImmersiveMode(struct android_app* state) {
+	JNIEnv* env{};
+	state->activity->vm->AttachCurrentThread(&env, NULL);
+
+	jclass activity_class = env->FindClass("android/app/NativeActivity");
+	jmethodID get_window = env->GetMethodID(
+		activity_class, "getWindow", "()Landroid/view/Window;");
+
+	jclass window_class = env->FindClass("android/view/Window");
+	jmethodID get_decor_view =
+		env->GetMethodID(window_class, "getDecorView", "()Landroid/view/View;");
+
+	jclass view_class = env->FindClass("android/view/View");
+	jmethodID set_system_ui_visibility =
+		env->GetMethodID(view_class, "setSystemUiVisibility", "(I)V");
+
+	jobject window = env->CallObjectMethod(state->activity->clazz, get_window);
+
+	jobject decor_view = env->CallObjectMethod(window, get_decor_view);
+
+	jfieldID flag_fullscreen_id =
+		env->GetStaticFieldID(view_class, "SYSTEM_UI_FLAG_FULLSCREEN", "I");
+	jfieldID flag_hide_navigation_id = env->GetStaticFieldID(
+		view_class, "SYSTEM_UI_FLAG_HIDE_NAVIGATION", "I");
+	jfieldID flag_immersive_sticky_id = env->GetStaticFieldID(
+		view_class, "SYSTEM_UI_FLAG_IMMERSIVE_STICKY", "I");
+
+	const int flag_fullscreen =
+		env->GetStaticIntField(view_class, flag_fullscreen_id);
+	const int flag_hide_navigation =
+		env->GetStaticIntField(view_class, flag_hide_navigation_id);
+	const int flag_immersive_sticky =
+		env->GetStaticIntField(view_class, flag_immersive_sticky_id);
+	const int flag =
+		flag_fullscreen | flag_hide_navigation | flag_immersive_sticky;
+
+	env->CallVoidMethod(decor_view, set_system_ui_visibility, flag);
+
+	state->activity->vm->DetachCurrentThread();
+}
+
+// NOLINTNEXTLINE defined name for Android native entry point.
+void android_main(struct android_app* state) {
+	enableImmersiveMode(state);
+
+	auto api = std::make_unique<API>();
+
+	initAndroidPlugin(*api, state);
+	initOpenGLPlugin(*api);
+	initSoLoudPlugin(*api);
+
+	if (auto err = static_cast<SystemAPIInternal&>(api->system()).init(); err) {
+		__android_log_print(
+			ANDROID_LOG_FATAL, "android_main", "Failed to init system API: %s",
+			err.get()->message().c_str());
+		exit(1);
+	}
+
+	if (auto err = static_cast<GraphicsAPIInternal&>(api->graphics()).init();
+		err) {
+		api->system().log(
+			LogLevel::FATAL, "android_main", "Failed to init graphics API: {}",
+			err.get()->message());
+		exit(2);
+	}
+	if (auto err = static_cast<AudioAPIInternal&>(api->audio()).init(); err) {
+		api->system().log(
+			LogLevel::FATAL, "android_main", "Failed to init audio API: {}",
+			err.get()->message());
+		exit(3);
+	}
+	api->system().log("android_main", "Android adapter created");
+
+	auto game = createGame();
+	game->setAPI(api.get());
+
+	api->system().log("android_main", "Game created");
+
+	// Wait for a window; process events.
+	while (!state->window) {
+		api->system().tick();
+	}
+
+	if (auto err = static_cast<GraphicsAPIInternal&>(api->graphics())
+					   .setWindow(WindowConfig{"", 0, 0, false});
+		err) {
+		api->system().log(
+			LogLevel::FATAL, "android_main", "Failed to create window: {}",
+			err.get()->message());
+		exit(4);
+	}
+
+	api->system().log("android_main", "Window created");
+
+	if (auto err = game->init(); err) {
+		api->system().log(
+			LogLevel::FATAL, "android_main", "Failed to init game: {}",
+			err.get()->message());
+		exit(5);
+	}
+
+	api->system().log("android_main", "Run!");
+	while (static_cast<SystemAPIInternal&>(api->system()).isRunning()) {
+		api->system().tick();
+		static_cast<GraphicsAPIInternal&>(api->graphics()).begin();
+		game->render();
+		static_cast<GraphicsAPIInternal&>(api->graphics()).end();
+	}
+
+	if (auto err = game->dispose(); err) {
+		api->system().log(
+			LogLevel::FATAL, "android_main", "Failed to dispose game: {}",
+			err.get()->message());
+		exit(6);
+	}
+	api->system().log("android_main", "Android adapter destroying");
+	static_cast<AudioAPIInternal&>(api->audio()).dispose();
+	static_cast<GraphicsAPIInternal&>(api->graphics()).dispose();
+	static_cast<SystemAPIInternal&>(api->system()).dispose();
+}
