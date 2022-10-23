ed5ac1ee082889a6b3ab55898d1e10df5e7556fb
Joel Auterson <joel.auterson@gmail.com>
2022-10-23 17:52:07 +0100

    Fix APK script

--- example/test_app/scripts/android_apk.sh
+++ example/test_app/scripts/android_apk.sh
@@ -5,7 +5,7 @@ set -xe
 
 make -j 8 
 mkdir -p apk/lib/armeabi-v7a
-mkdir -p apk/lib/assets
+mkdir -p apk/assets
 cp libgrowl-test-app.so apk/lib/armeabi-v7a/
 cp assets.growl apk/assets/
 "${ANDROID_SDK}"/build-tools/30.0.3/aapt package -f -M ../AndroidManifest.xml -I "${ANDROID_SDK}"/platforms/android-33/android.jar -S ../res/android -F apk-unaligned.apk apk
