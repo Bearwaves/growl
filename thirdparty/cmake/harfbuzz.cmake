46aa98e3518beebf05b28703cb3b1f109f8e3053
Joel Auterson <joel.auterson@gmail.com>
2022-10-28 19:23:06 +0100

    Formatting

--- thirdparty/cmake/harfbuzz.cmake
+++ thirdparty/cmake/harfbuzz.cmake
@@ -3,6 +3,6 @@ set(SOURCE_PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/harfbuzz)
 growl_thirdparty_lib(harfbuzz
 	SOURCES "${SOURCE_PREFIX}/src/harfbuzz.cc"
 	INCLUDES PUBLIC "${SOURCE_PREFIX}/src"
-	DEFINITIONS "HAVE_FREETYPE"
+	DEFINITIONS HAVE_FREETYPE
 	LINK growl-thirdparty::freetype
 	)
