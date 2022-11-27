12a84bdb03eb0717a44e8f82e59ccf3c4c0489e9
Joel Auterson <joel.auterson@gmail.com>
2022-11-27 22:59:48 +0000

    Make resize default false

--- src/core/include/growl/core/api/system_api.h
+++ src/core/include/growl/core/api/system_api.h
@@ -26,7 +26,9 @@ public:
 	virtual void setInputProcessor(InputProcessor* processor) {
 		inputProcessor = processor;
 	}
-	virtual bool didResize(int* width, int* height) = 0;
+	virtual bool didResize(int* width, int* height) {
+		return false;
+	}
 
 	template <class... Args>
 	void
