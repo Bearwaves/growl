e8f28d91c0ea38b238828a906b32adbaac31764d
Joel Auterson <joel.auterson@gmail.com>
2024-01-07 17:22:57 +0000

    Make Lua scripting stuff concret/runtime

--- src/core/include/growl/core/scripting/class.h
+++ src/core/include/growl/core/scripting/class.h
@@ -6,6 +6,58 @@
 
 namespace Growl {
 
+namespace {
+template <typename>
+struct ScriptingTypeOfType;
+
+template <typename T>
+struct ScriptingTypeOfType<const T&> {
+	static ScriptingType value() {
+		return ScriptingTypeOfType<T>::value();
+	}
+};
+
+template <>
+struct ScriptingTypeOfType<std::string_view> {
+	static ScriptingType value() {
+		return ScriptingType::String;
+	}
+};
+
+template <>
+struct ScriptingTypeOfType<int> {
+	static ScriptingType value() {
+		return ScriptingType::Int;
+	}
+};
+
+template <typename T>
+struct ScriptingTypeOfType<T*> {
+	static ScriptingType value() {
+		return ScriptingType::Ptr;
+	}
+};
+
+template <>
+struct ScriptingTypeOfType<void> {
+	static ScriptingType value() {
+		return ScriptingType::Void;
+	}
+};
+
+template <typename>
+struct GetFunctionSignature;
+template <typename T, typename... Args>
+struct GetFunctionSignature<T(Args...)> {
+	static const ScriptingSignature& value() {
+		static const ScriptingSignature v{
+			ScriptingTypeOfType<T>::value(),
+			std::vector<ScriptingType>{ScriptingTypeOfType<Args>::value()...}};
+		return v;
+	}
+};
+} // namespace
+
 class Class {
 public:
 	Class(std::string&& name, ScriptingAPI* lang)
@@ -19,11 +71,10 @@ public:
 	Class(Class&&) = default;
 	Class& operator=(Class&&) = default;
 
-	template <typename T, typename Context, typename... Args>
-	Error addMethod(
-		const std::string& name,
-		ScriptingAPI::ScriptingFn<T, Context, Args...> fn, Context* context) {
-		return lang->addMethodToClass(this->name, name, fn, context);
+	template <typename T, typename... Args>
+	Error addMethod(const std::string& name, ScriptingFn fn, void* context) {
+		auto signature = GetFunctionSignature<T(Args...)>::value();
+		return lang->addMethodToClass(this->name, name, signature, fn, context);
 	}
 
 protected:
