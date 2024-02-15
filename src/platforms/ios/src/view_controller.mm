5fee7afa4b53bf61f20e0c2c29ca4f63c6d6396f
Joel Auterson <joel.auterson@gmail.com>
2024-02-15 22:02:42 +0000

    Fix iOS build

--- src/platforms/ios/src/view_controller.mm
+++ src/platforms/ios/src/view_controller.mm
@@ -4,6 +4,7 @@
 #import "growl/core/graphics/window.h"
 #import "growl/platforms/ios/metal_view.h"
 #import "growl/scene/scene.h"
+#import <iostream>
 #import <memory>
 
 // Forward declarations for the linker.
