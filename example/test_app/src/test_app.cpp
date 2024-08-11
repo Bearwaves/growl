80a9802e1a576422d02b4e27e4523edb99f34544
Joel Auterson <joel.auterson@gmail.com>
2024-08-11 20:43:26 +0100

    Fixes

--- example/test_app/src/test_app.cpp
+++ example/test_app/src/test_app.cpp
@@ -35,7 +35,7 @@ int main(int argc, char** argv) {
 #elif GROWL_WEB
 	auto adapter = Growl::GameAdapter{
 		std::make_unique<Growl::TestAppGame>(),
-		Growl::WindowConfig{"Growl Test App", 1000, 1000, true}};
+		Growl::Config{"Growl Test App", 1000, 1000, true}};
 	adapter.run();
 #endif
 }
