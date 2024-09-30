d04e4c80f9e8fd1f530a81fee03a0dd7a5cd1424
Joel Auterson <joel.auterson@gmail.com>
2024-09-30 13:28:03 +0100

    Add hex-style Color constructor

--- src/core/include/growl/core/graphics/color.h
+++ src/core/include/growl/core/graphics/color.h
@@ -7,6 +7,22 @@ struct Color {
 	float b;
 	float a;
 
+	Color(float r, float g, float b, float a)
+		: r{r}
+		, g{g}
+		, b{b}
+		, a{a} {}
+
+	Color(float r, float g, float b)
+		: Color{r, g, b, 1.f} {}
+
+	Color(unsigned int hex_value) {
+		r = ((hex_value >> 16) & 0xFF) / 255.0;
+		g = ((hex_value >> 8) & 0xFF) / 255.0;
+		b = ((hex_value) & 0xFF) / 255.0;
+		a = 1;
+	}
+
 	float* data() {
 		return &r;
 	}
