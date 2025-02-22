82a433cfe5978c408726b8ff342e661376e30200
Joel Auterson <joel.auterson@gmail.com>
2025-02-22 17:46:12 +0000

    [Core] Add swing interpolations

--- src/core/include/growl/core/interpolation.h
+++ src/core/include/growl/core/interpolation.h
@@ -13,14 +13,17 @@ enum class Interpolation {
 	SineOut,
 	CircleIn,
 	CircleOut,
+	Swing,
+	SwingIn,
+	SwingOut,
 };
 
 float interpolate(
 	float from, float to, float factor,
-	Interpolation interpolation = Interpolation::Linear);
+	Interpolation interpolation = Interpolation::Linear, float scale = 1.f);
 
 Color interpolate(
 	Color from, Color to, float factor,
-	Interpolation interpolation = Interpolation::Linear);
+	Interpolation interpolation = Interpolation::Linear, float scale = 1.f);
 
 } // namespace Growl
--- src/core/src/interpolation.cpp
+++ src/core/src/interpolation.cpp
@@ -8,7 +8,8 @@ using Growl::Color;
 // https://github.com/libgdx/libgdx/blob/master/gdx/src/com/badlogic/gdx/math/Interpolation.java
 
 float Growl::interpolate(
-	float from, float to, float factor, Interpolation interpolation) {
+	float from, float to, float factor, Interpolation interpolation,
+	float scale) {
 	float a = factor;
 	switch (interpolation) {
 	case Interpolation::Linear:
@@ -36,6 +37,23 @@ float Growl::interpolate(
 		a--;
 		a = std::sqrt(1 - a * a);
 		break;
+	case Interpolation::Swing:
+		if (a <= 0.5f) {
+			a *= 2;
+			a = a * a * ((scale + 1) * a - scale) / 2;
+			break;
+		}
+		a--;
+		a *= 2;
+		a = a * a * ((scale + 1) * a + scale) / 2 + 1;
+		break;
+	case Interpolation::SwingIn:
+		a = a * a * ((scale + 1) * a - scale);
+		break;
+	case Interpolation::SwingOut:
+		a--;
+		a = a * a * ((scale + 1) * a + scale) + 1;
+		break;
 	default:
 		break;
 	}
@@ -43,11 +61,12 @@ float Growl::interpolate(
 }
 
 Color Growl::interpolate(
-	Color from, Color to, float factor, Interpolation interpolation) {
+	Color from, Color to, float factor, Interpolation interpolation,
+	float scale) {
 	return Color{
-		interpolate(from.r, to.r, factor, interpolation),
-		interpolate(from.g, to.g, factor, interpolation),
-		interpolate(from.b, to.b, factor, interpolation),
-		interpolate(from.a, to.a, factor, interpolation),
+		interpolate(from.r, to.r, factor, interpolation, scale),
+		interpolate(from.g, to.g, factor, interpolation, scale),
+		interpolate(from.b, to.b, factor, interpolation, scale),
+		interpolate(from.a, to.a, factor, interpolation, scale),
 	};
 }
