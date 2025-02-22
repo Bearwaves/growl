9e328147278c09408078097a0cc11966cf2bc4da
Joel Auterson <joel.auterson@gmail.com>
2025-02-22 17:49:31 +0000

    [Core] Add M_PI defines

--- src/core/src/interpolation.cpp
+++ src/core/src/interpolation.cpp
@@ -2,6 +2,11 @@
 #include "growl/core/graphics/color.h"
 #include <cmath>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288
+#define M_PI_2 1.57079632679489661923132169163975144
+#endif
+
 using Growl::Color;
 
 // From LibGDX
