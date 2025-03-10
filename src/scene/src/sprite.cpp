85ee489f214abbe032f8bae3b1684a0d3c2ad06f
Joel Auterson <joel.auterson@gmail.com>
2025-03-10 23:52:42 +0000

    [Scene] Use parent_alpha in sprite

--- src/scene/src/sprite.cpp
+++ src/scene/src/sprite.cpp
@@ -1,15 +1,20 @@
 #include "growl/scene/sprite.h"
 #include "growl/core/graphics/batch.h"
+#include "growl/core/graphics/color.h"
 
 using Growl::Batch;
+using Growl::Color;
 using Growl::Sprite;
 
 void Sprite::onDraw(Batch& batch, float parent_alpha, glm::mat4x4 transform) {
+	Color c = batch.getColor();
+	batch.setColor(c.multiplyAlpha(parent_alpha));
 	if (region) {
 		batch.draw(*region, 0, 0, getWidth(), getHeight(), transform);
 	} else if (texture) {
 		batch.draw(*texture, 0, 0, getWidth(), getHeight(), transform);
 	}
+	batch.setColor(c);
 }
 
 void Sprite::setRegion(TextureAtlasRegion&& region) {
