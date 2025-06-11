924819ed8dd41ef2e033ec9a00b6bb28a3fff20f
Joel Auterson <joel.auterson@gmail.com>
2025-06-12 00:17:51 +0100

    [Network] Make Future cancellable (#32)

--- src/core/include/growl/core/network/future.h
+++ src/core/include/growl/core/network/future.h
@@ -2,6 +2,7 @@
 
 #include "growl/core/error.h"
 #include <future>
+#include <memory>
 
 namespace Growl {
 
@@ -10,13 +11,46 @@ struct Future : public std::future<Result<T>> {
 	Future() noexcept
 		: std::future<Result<T>>() {}
 
-	Future(std::future<Result<T>> f) noexcept
-		: std::future<Result<T>>{std::move(f)} {}
+	~Future() {
+		if (this->valid() && !isReady()) {
+			cancel();
+		}
+	}
+
+	Future(const Future&) = delete;
+	Future& operator=(const Future&) = delete;
+	Future(Future&&) = default;
+	Future& operator=(Future&&) = default;
+
+	Future(
+		std::future<Result<T>> f,
+		std::shared_ptr<std::atomic<bool>> cancellation_token =
+			nullptr) noexcept
+		: std::future<Result<T>>{std::move(f)}
+		, cancellation_token{std::move(cancellation_token)} {}
+
+	Future(
+		std::future<Result<T>> f,
+		std::function<void()> cancellation_func) noexcept
+		: std::future<Result<T>>{std::move(f)}
+		, cancellation_func{std::move(cancellation_func)} {}
 
 	bool isReady() {
 		return this->wait_for(std::chrono::seconds(0)) ==
 			   std::future_status::ready;
 	}
+
+	void cancel() {
+		if (cancellation_token) {
+			cancellation_token->store(true);
+		} else if (cancellation_func) {
+			cancellation_func();
+		}
+	}
+
+private:
+	std::shared_ptr<std::atomic_bool> cancellation_token = nullptr;
+	std::function<void()> cancellation_func;
 };
 
 } // namespace Growl
--- src/plugins/curl/src/curl_network.cpp
+++ src/plugins/curl/src/curl_network.cpp
@@ -4,6 +4,7 @@
 #include "growl/core/api/api.h"
 #include "growl/core/api/api_internal.h"
 #include "growl/core/network/future.h"
+#include <atomic>
 
 using Growl::API;
 using Growl::Config;
@@ -46,40 +47,75 @@ size_t writeToString(void* contents, size_t size, size_t n, void* user) {
 	return real_size;
 }
 
+int progressCallback(
+	void* clientp, curl_off_t, curl_off_t, curl_off_t, curl_off_t) {
+	auto cancellation_token = static_cast<std::atomic_bool*>(clientp);
+	return cancellation_token->load() ? 1 : 0;
+}
+
 Future<HttpResponse>
 CurlNetworkAPI::doHttpRequest(std::unique_ptr<HttpRequest> request) {
-	return std::async(
-		std::launch::async,
-		[req = std::move(request)]() -> Result<HttpResponse> {
-			CURL* c = static_cast<CurlHttpRequest*>(req.get())->getRequest();
-			HttpResponse response;
+	auto cancellation_token = std::make_shared<std::atomic_bool>(false);
+	return Future(
+		std::async(
+			std::launch::async,
+			[req = std::move(request),
+			 cancellation_token]() -> Result<HttpResponse> {
+				CURL* c =
+					static_cast<CurlHttpRequest*>(req.get())->getRequest();
+				HttpResponse response;
 
-			if (auto err =
-					curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, writeToString);
-				err != CURLE_OK) {
-				return Error(
-					std::make_unique<CurlError>(
-						"Failed to set response write function", err));
-			}
-			if (auto err =
-					curl_easy_setopt(c, CURLOPT_WRITEDATA, &(response.content));
-				err != CURLE_OK) {
-				return Error(
-					std::make_unique<CurlError>(
-						"Failed to set response write pointer", err));
-			}
+				if (auto err = curl_easy_setopt(
+						c, CURLOPT_WRITEFUNCTION, writeToString);
+					err != CURLE_OK) {
+					return Error(
+						std::make_unique<CurlError>(
+							"Failed to set response write function", err));
+				}
+				if (auto err = curl_easy_setopt(
+						c, CURLOPT_WRITEDATA, &(response.content));
+					err != CURLE_OK) {
+					return Error(
+						std::make_unique<CurlError>(
+							"Failed to set response write pointer", err));
+				}
 
-			if (auto err = curl_easy_perform(c); err != CURLE_OK) {
-				return Error(
-					std::make_unique<CurlError>("Failed to make request", err));
-			}
-			if (auto err = curl_easy_getinfo(
-					c, CURLINFO_RESPONSE_CODE, &(response.status_code));
-				err != CURLE_OK) {
-				return Error(
-					std::make_unique<CurlError>(
-						"Failed to read response status code", err));
-			}
-			return response;
-		});
+				if (auto err = curl_easy_setopt(c, CURLOPT_NOPROGRESS, 0L);
+					err != CURLE_OK) {
+					return Error(
+						std::make_unique<CurlError>(
+							"Failed to set noprogress", err));
+				}
+
+				if (auto err = curl_easy_setopt(
+						c, CURLOPT_XFERINFODATA, cancellation_token.get());
+					err != CURLE_OK) {
+					return Error(
+						std::make_unique<CurlError>(
+							"Failed to set progress data", err));
+				}
+
+				if (auto err = curl_easy_setopt(
+						c, CURLOPT_XFERINFOFUNCTION, progressCallback);
+					err != CURLE_OK) {
+					return Error(
+						std::make_unique<CurlError>(
+							"Failed to set progress data", err));
+				}
+
+				if (auto err = curl_easy_perform(c); err != CURLE_OK) {
+					return Error(
+						std::make_unique<CurlError>(
+							"Failed to make request", err));
+				}
+				if (auto err = curl_easy_getinfo(
+						c, CURLINFO_RESPONSE_CODE, &(response.status_code));
+					err != CURLE_OK) {
+					return Error(
+						std::make_unique<CurlError>(
+							"Failed to read response status code", err));
+				}
+				return response;
+			}),
+		std::move(cancellation_token));
 }
