d1d7797f0129a5b02d8d902675738a7459962278
Joel Auterson <joel.auterson@gmail.com>
2025-09-11 19:10:23 +0100

    [Build] Support macOS universal binaries

--- thirdparty/cmake/mbedtls.cmake
+++ thirdparty/cmake/mbedtls.cmake
@@ -2,10 +2,6 @@ set(ENABLE_TESTING OFF CACHE INTERNAL "Do not build tests")
 set(ENABLE_PROGRAMS OFF CACHE INTERNAL "Do not build tools")
 add_subdirectory(mbedtls)
 
-if (APPLE)
-	set(CMAKE_C_ARCHIVE_FINISH "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
-endif()
-
 set(MBEDTLS_LIBRARY mbedtls)
 set(MBEDX509_LIBRARY mbedx509)
 set(MBEDCRYPTO_LIBRARY mbedcrypto)
