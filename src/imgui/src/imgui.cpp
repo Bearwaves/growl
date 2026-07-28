156c7756bf86d3bbded3a6aea3872c4e35186a60
Joel Auterson <joel.auterson@gmail.com>
2026-07-28 22:50:23 +0100

    [Imgui] Don't hide tab bar

--- src/imgui/src/imgui.cpp
+++ src/imgui/src/imgui.cpp
@@ -69,8 +69,7 @@ void Growl::imGuiBegin(API& api) {
 	dockspace_id = ImGui::GetID("DockSpace");
 	if (needs_setup) {
 		needs_setup = !ImGui::DockBuilderGetNode(dockspace_id);
-		ImGui::DockSpaceOverViewport(
-			dockspace_id, nullptr, ImGuiDockNodeFlags_HiddenTabBar);
+		ImGui::DockSpaceOverViewport(dockspace_id);
 		if (needs_setup) {
 			api.system().log("ImGui", "Dock space needs first time setup");
 			ImGui::DockBuilderDockWindow("Game", dockspace_id);
@@ -84,8 +83,7 @@ void Growl::imGuiBegin(API& api) {
 			ImGui::DockBuilderFinish(dockspace_id);
 		}
 	} else {
-		ImGui::DockSpaceOverViewport(
-			dockspace_id, nullptr, ImGuiDockNodeFlags_HiddenTabBar);
+		ImGui::DockSpaceOverViewport(dockspace_id);
 	}
 	doApiWindows(api);
 }
